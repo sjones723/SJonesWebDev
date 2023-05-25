1e2bdff1550988239d25f82dfc57d57b2d684d28
Sydney Jones <sydneyjones87@gmail.com>
2023-05-25 15:36:02 -0500

    resume outline

--- resume/src/app/app.module.ts
+++ resume/src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes, ExtraOptions, InitialNavigation } from '@angular/router'; // Import InitialNavigation
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactComponent } from './profile/contact/contact.component';
+import { EducationComponent } from './profile/education/education.component';
+import { ExperienceComponent } from './profile/experience/experience.component';
+import { FooterComponent } from './profile/footer/footer.component';
+import { HeaderComponent } from './profile/header/header.component';
+import { IntroComponent } from './profile/intro/intro.component';
+import { PortfolioComponent } from './profile/portfolio/portfolio.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ProfileModule } from './profile/profile.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled' as InitialNavigation // Assign the value as InitialNavigation
+    }),
+    ProfileModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
